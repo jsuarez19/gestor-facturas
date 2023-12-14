74b7ca40e39f366094acb5d00610156595ad7647
yerti <yerti0818@gmail.com>
2023-12-14 10:06:12 -0500

    Feat: files of javascript

---
+++ frontend-gestor-facturas/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Titulo from './components/Titulo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world !</h1>
+      <Titulo></Titulo>
+    </div>
+  );
+}
+
+export default App;
