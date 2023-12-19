cfa841914916ae1b73db83998e8bc13ad29b7447
Jose Suarez <jolusuvi@gmail.com>
2023-12-18 23:39:33 -0500

    Configurar método PUT para que se considere solamente los campos que deben ser editables

--- gestion_facturas/api/models.py
+++ gestion_facturas/api/models.py
@@ -2,21 +2,22 @@ from django.db import models
 
 class DocumentoVenta(models.Model):
     ID = models.AutoField(primary_key=True)
-    NUMERO_FACTURA = models.CharField(max_length=20, null=True)
-    FECHA_EMISION = models.DateField(null=True)
-    MONEDA = models.CharField(max_length=60, null=True)
-    MONTO = models.DecimalField(max_digits=18, decimal_places=2, null=True)
-    TIPO_CLIENTE = models.CharField(max_length=60, null=True)
-    NOMBRE_CLIENTE = models.CharField(max_length=150, null=True)
-    CONDICION_PAGO = models.CharField(max_length=60, null=True)
-    VENDEDOR = models.CharField(max_length=60, null=True)
-    CANCELADO = models.CharField(max_length=1, null=True)
-    ESTADO = models.CharField(max_length=3, null=True)
-    ESTATUS = models.CharField(max_length=20, default="Emitido")
-    PERSONAL_ASIGNADO = models.CharField(max_length=80, null=True)
-    FECHA_ENTREGA = models.DateField(null=True)
-    COMENTARIOS = models.CharField(max_length=400, null=True)
-    DATOS_ADJUNTOS = models.BinaryField(null=True)
+    NUMERO_FACTURA = models.CharField(max_length=20, null=True, editable=False)
+    FECHA_EMISION = models.DateField(null=True, editable=False)
+    MONEDA = models.CharField(max_length=60, null=True, editable=False)
+    MONTO = models.DecimalField(max_digits=18, decimal_places=2, null=True, editable=False)
+    TIPO_CLIENTE = models.CharField(max_length=60, null=True, editable=False)
+    NOMBRE_CLIENTE = models.CharField(max_length=150, null=True, editable=False)
+    CONDICION_PAGO = models.CharField(max_length=60, null=True, editable=False)
+    VENDEDOR = models.CharField(max_length=60, null=True, editable=False)
+    CANCELADO = models.CharField(max_length=1, null=True, editable=False)
+    ESTADO = models.CharField(max_length=3, null=True, editable=False)
+    # Modificables por el aplicativo
+    ESTATUS = models.CharField(max_length=20, default="Emitido", editable=True)
+    PERSONAL_ASIGNADO = models.CharField(max_length=80, null=True, editable=True)
+    FECHA_ENTREGA = models.DateField(null=True, editable=True)
+    COMENTARIOS = models.CharField(max_length=400, null=True, editable=True)
+    DATOS_ADJUNTOS = models.BinaryField(null=True, editable=True)
 
     def __str__(self):
         return f"{self.NUMERO_FACTURA} - {self.NOMBRE_CLIENTE}"
--- gestion_facturas/api/views.py
+++ gestion_facturas/api/views.py
@@ -9,6 +9,16 @@ from .serializers import DocumentoVentaSerializer
 class DocumentoVentaViewSet(viewsets.ModelViewSet):
     queryset = DocumentoVenta.objects.all()
     serializer_class = DocumentoVentaSerializer
+    http_method_names = ['get', 'post', 'head', 'put', 'patch']
+
+def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
 
 def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
