fcd39a69a04159f08bf6681e72b0fbac9f7cbe2d
Jose Suarez <jolusuvi@gmail.com>
2023-12-18 23:59:24 -0500

    Para enviar solamente información necesaria a través del GET

--- gestion_facturas/api/serializers.py
+++ gestion_facturas/api/serializers.py
@@ -4,4 +4,4 @@ from .models import DocumentoVenta
 class DocumentoVentaSerializer(serializers.ModelSerializer):
     class Meta:
         model = DocumentoVenta
-        fields = '__all__'
+        fields = ['NUMERO_FACTURA', 'FECHA_EMISION', 'ESTATUS', 'PERSONAL_ASIGNADO', 'NOMBRE_CLIENTE', 'FECHA_ENTREGA', 'CANCELADO', 'ESTADO']
